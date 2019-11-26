bd74f6936d239757fac600bdf3ef623e5358a07e
banglekim <banglekim.hp@gmail.com>
2019-11-25 22:41:16 -0600

    Added code to check execution time of each method.

--- src/main/java/com/bangle/weather/Aspects/Loggable.java
+++ src/main/java/com/bangle/weather/Aspects/Loggable.java
@@ -1,4 +1,11 @@
 package com.bangle.weather.Aspects;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Loggable {
 }
--- src/main/java/com/bangle/weather/Aspects/LoggingAspect.java
+++ src/main/java/com/bangle/weather/Aspects/LoggingAspect.java
@@ -1,4 +1,43 @@
 package com.bangle.weather.Aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
 public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("@annotation(Loggable)")
+    public void executeLogging() {
+    }
+
+    @Around(value = "executeLogging()")
+    public Object logMethodCalled( ProceedingJoinPoint joinPoint ) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object returnValue= joinPoint.proceed();
+        long duration = System.currentTimeMillis() - startTime;
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName()).append(", ");
+        message.append("execution time = ").append(duration).append("ms, ");
+        Object[] args = joinPoint.getArgs();
+        if (args!=null && args.length>0) {
+            message.append("args =[");
+            Arrays.asList(args).forEach(arg -> {
+                message.append("arg=").append(arg).append(" | ");
+            });
+            message.append("]");
+        }
+        LOGGER.info(message.toString());
+        return returnValue;
+    }
+
 }
